7e711abf5af791d2937dcff0f3d9d93308b3950a
Sultanmp <sultanmuhammadpraja@gmail.com>
2022-09-14 15:01:48 +0700

    Latihan

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
 function main() {
-    var kanvas = document.getElementById("kanvas");
-    var gl = kanvas.getContext("webgl");
+  var kanvas = document.getElementById('kanvas');
+  var gl = kanvas.getContext('webgl');
 
-    // Vertex shader
-    var vertexShaderCode = 
-    "void main() {" + 
-    "}";
-    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShaderObject, vertexShaderCode);
-    gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
-
-    // Fragment shader
-    var fragmentShaderCode = `
+  // Vertex shader
+  var vertexShaderCode = `
     void main() {
-
+        float x = 0.0;
+        float y = 0.0;
+        float z = 0.0;
+        float w = 1.0;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, z, w);
     }
     `;
-    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.compileShader(fragmentShaderObject);   // sampai sini sudah jadi .o
+  var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShaderObject, vertexShaderCode);
+  gl.compileShader(vertexShaderObject); // sampai sini sudah jadi .o
 
-    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
-    gl.attachShader(shaderProgram, vertexShaderObject);
-    gl.attachShader(shaderProgram, fragmentShaderObject);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
+  // Fragment shader
+  var fragmentShaderCode = `
+  precision highp float;
+    void main() {
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
+    }
+    `;
+  var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+  gl.compileShader(fragmentShaderObject); // sampai sini sudah jadi .o
 
-    gl.clearColor(1.0,      0.65,    0.0,    1.0);
-    //            Merah     Hijau   Biru    Transparansi
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
+  gl.attachShader(shaderProgram, vertexShaderObject);
+  gl.attachShader(shaderProgram, fragmentShaderObject);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  gl.clearColor(1.0, 0.65, 0.0, 1.0); // Oranye
+  //            Merah     Hijau   Biru    Transparansi
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
